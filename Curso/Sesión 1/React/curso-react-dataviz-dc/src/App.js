a084fdd0ab41f6989f4f7a28b0f39ee611763a1f
Jorge Andrade <jorandradefig@gmail.com>
2022-06-23 18:54:11 -0500

    Sesión 4

--- Curso/Sesión 1/React/curso-react-dataviz-dc/src/App.js
+++ Curso/Sesión 1/React/curso-react-dataviz-dc/src/App.js
@@ -1,99 +1,17 @@
-import styled from "styled-components";
+import Home from "./views/Home";
+import Demo from "./views/Demo";
 
-import logo from "./logo.svg";
-import loQueDiga, { funcionQueRegresaHola as hola } from "./utils/funciones";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-// Componentes
-import Button from "./components/Button/Button";
-import { useState } from "react";
-
-const StyledButton = styled.button`
-  border: 1px solid black;
-  background-color: #eeeeee;
-  border-radius: 25px;
-  font-weight: bold;
-  padding: 20px 15px;
-`;
-
-const StyledAnchor = styled.a`
-  ${({ withBackground }) =>
-    withBackground
-      ? "background-color: " + withBackground
-      : "background-color: red"};
-  border: 4px solid ${(props) => props.color};
-
-  &:hover {
-    color: white;
-  }
-
-  & .text {
-    color: green;
-
-    &:hover {
-      color: pink;
-    }
-  }
-`;
-
-function App() {
-  const [hideButton, setHideButton] = useState(false);
-
-  const nombre = "Data Cívica";
-  const str = `Hola, ${nombre}`;
-  const etiquetas = {
-    etiqueta: "hola",
-    etiqueta2: "adiós",
-  };
-
-  console.log(str);
-
+const App = () => {
   return (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            ¡{loQueDiga("Hey,")} {hola()} {nombre}!
-          </p>
-          <a
-            data-toggle="tooltip"
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <StyledButton onClick={() => setHideButton(!hideButton)}>
-            Este es un botón con <code>styled-components</code>
-          </StyledButton>
-          {!hideButton &&
-          <Button {...etiquetas}>
-            <ul>
-              <li>Hola</li>
-            </ul>
-          </Button>}
-          <StyledAnchor withBackground="transparent" color="cyan">
-            <span className="text">Link vacío</span>
-          </StyledAnchor>
-          <StyledAnchor withBackground="transparent" color="cyan">
-            <span className="text">Link vacío</span>
-          </StyledAnchor>
-          <StyledAnchor withBackground="transparent" color="cyan">
-            <span>Link vacío</span>
-          </StyledAnchor>
-          <button className="rounded-full bg-red-400">
-            Botón redondeado con Tailwind
-          </button>
-        </header>
-      </div>
-      <div>Adiós</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="demo" element={<Demo />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ Curso/Sesión 1/React/curso-react-dataviz-dc/src/views/Home.js
@@ -0,0 +1,201 @@
+// MVC -> Model - View - Controller
+import { ResponsiveStream } from "@nivo/stream";
+
+const MyResponsiveStream = () => {
+  const data = [
+    {
+      Raoul: 38,
+      Josiane: 122,
+      Marcel: 69,
+      René: 113,
+      Paul: 196,
+      Jacques: 126,
+    },
+    {
+      Raoul: 56,
+      Josiane: 81,
+      Marcel: 10,
+      René: 157,
+      Paul: 10,
+      Jacques: 23,
+    },
+    {
+      Raoul: 84,
+      Josiane: 24,
+      Marcel: 119,
+      René: 195,
+      Paul: 107,
+      Jacques: 59,
+    },
+    {
+      Raoul: 164,
+      Josiane: 20,
+      Marcel: 123,
+      René: 60,
+      Paul: 99,
+      Jacques: 103,
+    },
+    {
+      Raoul: 86,
+      Josiane: 33,
+      Marcel: 50,
+      René: 91,
+      Paul: 69,
+      Jacques: 76,
+    },
+    {
+      Raoul: 199,
+      Josiane: 180,
+      Marcel: 176,
+      René: 198,
+      Paul: 84,
+      Jacques: 76,
+    },
+    {
+      Raoul: 34,
+      Josiane: 114,
+      Marcel: 160,
+      René: 178,
+      Paul: 46,
+      Jacques: 162,
+    },
+    {
+      Raoul: 188,
+      Josiane: 62,
+      Marcel: 195,
+      René: 177,
+      Paul: 96,
+      Jacques: 43,
+    },
+    {
+      Raoul: 48,
+      Josiane: 181,
+      Marcel: 118,
+      René: 113,
+      Paul: 18,
+      Jacques: 108,
+    },
+  ];
+
+  return (
+    <ResponsiveStream
+      data={data}
+      keys={["Raoul", "Josiane", "Marcel", "René", "Paul", "Jacques"]}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendOffset: 36,
+      }}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendOffset: -40,
+      }}
+      enableGridX={true}
+      enableGridY={false}
+      offsetType="silhouette"
+      colors={{ scheme: "nivo" }}
+      fillOpacity={0.85}
+      borderColor={{ theme: "background" }}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "#2c998f",
+          size: 4,
+          padding: 2,
+          stagger: true,
+        },
+        {
+          id: "squares",
+          type: "patternSquares",
+          background: "inherit",
+          color: "#e4c912",
+          size: 6,
+          padding: 2,
+          stagger: true,
+        },
+      ]}
+      fill={[
+        {
+          match: {
+            id: "Paul",
+          },
+          id: "dots",
+        },
+        {
+          match: {
+            id: "Marcel",
+          },
+          id: "squares",
+        },
+      ]}
+      dotSize={8}
+      dotColor={{ from: "color" }}
+      dotBorderWidth={2}
+      dotBorderColor={{
+        from: "color",
+        modifiers: [["darker", 0.7]],
+      }}
+      legends={[
+        {
+          anchor: "bottom-right",
+          direction: "column",
+          translateX: 100,
+          itemWidth: 80,
+          itemHeight: 20,
+          itemTextColor: "#999999",
+          symbolSize: 12,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#000000",
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
+
+const Home = () => {
+  return (
+    <div data-theme="cupcake">
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn bg-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
